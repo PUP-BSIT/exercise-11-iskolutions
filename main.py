b3cc0605da6ea01a1ef886ea0a38554a73ba733f
causon-mikolorenz <causonmikolorenz@gmail.com>
2025-05-14 20:34:45 +0800

    add process_choice function

--- main.py
+++ main.py
@@ -22,10 +22,27 @@ def display_get_choice():
         print("Invalid input! Please enter a number.")
         return UNSET_OPTION
 
+def process_choice(choice):
+    match choice:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            pass
+        case _:
+            pass
 
 def main():
     choice = UNSET_OPTION
     while choice != EXIT_OPTION:
         choice = display_get_choice()
+        process_choice(choice)
 
 main()
